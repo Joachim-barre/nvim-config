29702516035df7479a4b68c211fd42d545b6d3e3
Joachim-barre <joachim.barre22@laposte.net>
2024-06-09 16:25:56 +0200

    fixed omnisharpcofing

--- lua/lsp-setup.lua
+++ lua/lsp-setup.lua
@@ -38,9 +38,16 @@ function M.setup()
         }))
     end
     if config.csharp then
-        require('lspconfig').omnisharp.setup{
-            
-        }
+            require("lspconfig").omnisharp.setup {
+                capabilities = capabilities,
+                cmd = { "dotnet", vim.fn.stdpath "data" .. "/mason/packages/omnisharp/libexec/OmniSharp.dll" },
+                enable_import_completion = true,
+                organize_imports_on_format = true,
+                enable_roslyn_analyzers = true,
+                root_dir = function ()
+                    return vim.loop.cwd() -- current working directory
+                end,
+            }
     end
     if config.rust then
         require('lspconfig').rust_analyzer.setup{
