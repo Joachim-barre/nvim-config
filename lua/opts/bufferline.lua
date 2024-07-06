92db7d2d68e87c4475572faee6e73a82cd5d8194
Joachim-barre <joachim.barre22@laposte.net>
2024-07-06 15:01:54 +0200

    changed bufferline style

--- lua/opts/bufferline.lua
+++ lua/opts/bufferline.lua
@@ -1,3 +1,19 @@
+ vim.cmd([[
+
+augroup BufferLineColor
+
+autocmd!
+
+autocmd VimEnter * highlight BufferLineFill guibg=#191724
+
+autocmd VimEnter * highlight BufferLineSeparator guifg=#191724
+
+autocmd VimEnter * highlight BufferLineSeparatorSelected guifg=#191724
+
+augroup END
+
+]]) 
+
 return {
     options = {
         diagnostics = "nvim_lsp",
@@ -10,7 +26,8 @@ return {
         },
         close_command = function(bufnum)
             require('bufdelete').bufdelete(bufnum, true)
-        end
+        end,
+        separator_style="slant"
     }
 }
 
