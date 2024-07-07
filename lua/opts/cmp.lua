4c21d198ee31b861fdade129445caeac69c750b0
Joachim-barre <joachim.barre22@laposte.net>
2024-07-07 18:00:27 +0200

    changed keymaps

--- lua/opts/cmp.lua
+++ lua/opts/cmp.lua
@@ -1,14 +1,20 @@
 cmp = require("cmp")
 
 cmp.setup.cmdline({ '/', '?' }, {
-mapping = cmp.mapping.preset.cmdline(),
+mapping = cmp.mapping.preset.cmdline({
+    ['<Down>'] = { c = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }) },
+    ['<Up>'] = { c = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }) },
+}),
 sources = {
     { name = 'buffer' }
 }})
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(':', {
-mapping = cmp.mapping.preset.cmdline(),
+mapping = cmp.mapping.preset.cmdline({
+    ['<Down>'] = { c = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }) },
+    ['<Up>'] = { c = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }) },
+}),
 sources = {
     { name = 'path' },
     { name = 'cmdline' }
@@ -20,10 +26,11 @@ vim.opt.completeopt = "menu,menuone"
 
 return {
     mapping = cmp.mapping.preset.insert({
-      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.abort(),
+      ['<S-Down>'] = cmp.mapping.scroll_docs(-4),
+      ['<S-Up>'] = cmp.mapping.scroll_docs(4),
+      ['<Down>'] = { c = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }) },
+      ['<Up>'] = { c = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }) },
+      ['<Esc>'] = cmp.mapping.abort(),
       ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
     sources = {
