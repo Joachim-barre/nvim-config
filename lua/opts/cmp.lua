fb5259694bd44cb00aacb813c9555326ad7f352a
Joachim-barre <joachim.barre22@laposte.net>
2024-07-07 17:50:55 +0200

    created basic config

---
+++ lua/opts/cmp.lua
@@ -0,0 +1,33 @@
+cmp = require("cmp")
+
+cmp.setup.cmdline({ '/', '?' }, {
+mapping = cmp.mapping.preset.cmdline(),
+sources = {
+    { name = 'buffer' }
+}})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+mapping = cmp.mapping.preset.cmdline(),
+sources = {
+    { name = 'path' },
+    { name = 'cmdline' }
+},
+matching = { disallow_symbol_nonprefix_matching = false }
+})
+
+vim.opt.completeopt = "menu,menuone"
+
+return {
+    mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = {
+        { name = 'diagraphs' },
+        { name = 'buffer' }
+    } 
+}
