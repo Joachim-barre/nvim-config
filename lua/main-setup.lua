fec376683acb32a661e8329e5642bc5cbd43cdf1
Joachim-barre <joachim.barre22@laposte.net>
2024-07-10 20:10:26 +0200

    fixed and reformated main config

--- lua/main-setup.lua
+++ lua/main-setup.lua
@@ -1,13 +1,19 @@
 safe_setup = require "safe_require".safe_setup
 safe_require = require "safe_require".safe_require 
+
 local status, profile = pcall(require("profile-loader").load)
+
 if not status then
-vim.notify("failed to load profile")
-local profile = require("profile-loader").load_default()
+    vim.notify("failed to load profile")
+    local profile = require("profile-loader").load_default()
 end
-for i, script in ipairs(profile["local-scripts"]) do
-    safe_require(script)
+
+if profile["local-scripts"] ~= nil then
+    for i, script in ipairs(profile["local-scripts"]) do
+        safe_require(script)
+    end
 end
+
 for i, module in ipairs(profile.modules) do
     safe_setup(module)
 end
