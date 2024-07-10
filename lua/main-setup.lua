9c5357ef7eab0b264f61fb9c8b0805b5e9a57568
Joachim-barre <joachim.barre22@laposte.net>
2024-07-10 19:14:28 +0200

    added local scripts loader

--- lua/main-setup.lua
+++ lua/main-setup.lua
@@ -4,13 +4,10 @@ local status, profile = pcall(require("profile-loader").load)
 if not status then
 vim.notify("failed to load profile")
 local profile = require("profile-loader").load_default()
-end 
+end
+for i, script in ipairs(profile["local-scripts"]) do
+    safe_require(script)
+end
 for i, module in ipairs(profile.modules) do
     safe_setup(module)
-end
-
-local_path = vim.fn.expand("./.nvim/local-conf.lua") 
-if vim.fn.filereadable(local_path) then
-    package.path = './.nvim/?.lua;' .. package.path 
-    safe_require("local-conf")
 end
