75ced283d4e09fb22ced3f0708da81263200151e
Joachim-barre <joachim.barre22@laposte.net>
2024-07-10 18:29:00 +0200

    added profile system

--- lua/main-setup.lua
+++ lua/main-setup.lua
@@ -1,35 +1,11 @@
 safe_setup = require "safe_require".safe_setup
-safe_require = require "safe_require".safe_require
-modules = {
-    "rocks-setup",
-    "sidebar-nvim",
-    "nvim-tree",
-    "bufferline",
-    "toggletasks",
-    "toggleterm",
-    "startup",
-    "telescope",
-    "symbols-outline",
-    "code_runner",
-    "session-setup",
-    "welcome-setup",
-    "statusline-setup",
-    "lsp-setup",
-    'nvim-treesitter.configs',
-    "dap-config",
-    "dapui",
-    "quickgd",
-    "mason",
-    "mason-lspconfig",
-    'mini.clue',
-    'venv-selector',
-    'glow',
-    'cmp',
-    'gitsigns',
-    'mini.git',
-    'render-markdown'
-}
-for i, module in ipairs(modules) do
+safe_require = require "safe_require".safe_require 
+local status, profile = pcall(require("profile-loader").load)
+if not status then
+vim.notify("failed to load profile")
+local profile = require("profile-loader").load_default()
+end 
+for i, module in ipairs(profile.modules) do
     safe_setup(module)
 end
 
---
+++ lua/profile-loader.lua
@@ -0,0 +1,66 @@
+local M = {}
+
+M.modules = {
+    "rocks-setup",
+    "sidebar-nvim",
+    "nvim-tree",
+    "bufferline",
+    "toggletasks",
+    "toggleterm",
+    "startup",
+    "telescope",
+    "symbols-outline",
+    "code_runner",
+    "session-setup",
+    "welcome-setup",
+    "statusline-setup",
+    "lsp-setup",
+    'nvim-treesitter.configs',
+    "dap-config",
+    "dapui",
+    "quickgd",
+    "mason",
+    "mason-lspconfig",
+    'mini.clue',
+    'venv-selector',
+    'glow',
+    'cmp',
+    'gitsigns',
+    'mini.git',
+    'render-markdown'
+}
+
+function build_modulelist()
+    local table1 = M.modules
+    local table2 = vim.g.profile.disabled_modules
+    -- https://stackoverflow.com/a/64590508
+    for number = 1, #table1 do
+        for delete = 1, #table2 do
+            if table1[number] == table2[delete] then
+                for i = number,  #table1 -1 do
+                    table1[i] = table1[i +1]
+                end  --  shuffle every entry in table down
+                table1 [#table1] = nil  --  erase last entry
+            end  --  number == delete
+        end  --  loop through table2
+    end  --  loop through table1
+    return table1
+end
+
+function apply_profile() 
+    local profile = vim.g.profile
+    profile.modules = build_modulelist()
+    vim.g.profile = profile
+end
+
+function M.load_default()
+    vim.g.profile = require("code_runner.load_json")(vim.fn.stdpath("config") .. "/defaults-configs/profile.json")
+    apply_profile()
+    return vim.g.profile
+end
+
+function M.load()
+    return M.load_default()
+end
+
+return M
