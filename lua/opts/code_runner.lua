1d844baf21e2ef4939d40c62713624a68a3f6e8f
Joachim-barre <joachim.barre22@laposte.net>
2024-07-11 16:50:51 +0200

    moved run configuration to profile.json

--- lua/opts/code_runner.lua
+++ lua/opts/code_runner.lua
@@ -8,36 +8,7 @@ local project_path = vim.fn.expand("./.nvim/project.json")
 if vim.fn.filereadable(project_path) == 1 then
     opts.project_path = project_path
 end
-local filetype_path = vim.fn.expand("./.nvim/run.json")
-filetype_default = {
-    java = {
-        "cd $dir &&",
-        "javac $fileName &&",
-        "java $fileNameWithoutExt"
-    },
-    python = 'python3 $file $end',
-    sh = "zsh",
-    c = {
-        "cd $dir &&",
-        "clang $fileName",
-        "-o $fileNameWithoutExt &&",
-        "$dir/$fileNameWithoutExt",
-    },
-    cpp = {
-        "cd $dir &&",
-        "clang $fileName",
-        "-o $fileNameWithoutExt &&",
-        "$dir/$fileNameWithoutExt",
-    },
-    rust = "cargo test && cargo run $end"
-}
-if vim.fn.filereadable(filetype_path) == 1 then
-    filetype = require("code_runner.load_json")(filetype_path)
-else
-    filetype = {}
-end
-for k,v in pairs(filetype) do filetype_default[k] = v end
-opts.filetype = filetype_default
+opts.filetype = vim.g.profile.run
 
 vim.keymap.set('n', '<leader>r', ':RunCode<CR>', { noremap = true, silent = false })
 
