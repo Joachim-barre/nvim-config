0210649f73a17b5abf574f4b9f1a402bb61daf40
Joachim-barre <joachim.barre22@laposte.net>
2024-08-16 15:27:56 +0200

    completly modified cmp menu

--- lua/opts/cmp.lua
+++ lua/opts/cmp.lua
@@ -26,44 +26,63 @@ matching = { disallow_symbol_nonprefix_matching = false }
 vim.opt.completeopt = "menu,menuone"
 
 vim.api.nvim_set_hl(0, "CmpNormal", { bg = "#131313" })
--- gray
-vim.api.nvim_set_hl(0, 'CmpItemAbbrDeprecated', { bg='NONE', strikethrough=true, fg='#808080' })
--- blue
-vim.api.nvim_set_hl(0, 'CmpItemAbbrMatch', { bg='NONE', fg='#569CD6' })
-vim.api.nvim_set_hl(0, 'CmpItemAbbrMatchFuzzy', { link='CmpIntemAbbrMatch' })
--- light blue
-vim.api.nvim_set_hl(0, 'CmpItemKindVariable', { bg='NONE', fg='#9CDCFE' })
-vim.api.nvim_set_hl(0, 'CmpItemKindInterface', { link='CmpItemKindVariable' })
-vim.api.nvim_set_hl(0, 'CmpItemKindText', { link='CmpItemKindVariable' })
--- pink
-vim.api.nvim_set_hl(0, 'CmpItemKindFunction', { bg='NONE', fg='#C586C0' })
-vim.api.nvim_set_hl(0, 'CmpItemKindMethod', { link='CmpItemKindFunction' })
--- front
-vim.api.nvim_set_hl(0, 'CmpItemKindKeyword', { bg='NONE', fg='#D4D4D4' })
-vim.api.nvim_set_hl(0, 'CmpItemKindProperty', { link='CmpItemKindKeyword' })
-vim.api.nvim_set_hl(0, 'CmpItemKindUnit', { link='CmpItemKindKeyword' })
+
+vim.api.nvim_set_hl(0, "CmpItemAbbrDeprecated", { fg = "#7E8294", bg = "NONE", strikethrough = true })
+vim.api.nvim_set_hl(0, "CmpItemAbbrMatch", { fg = "#82AAFF", bg = "NONE", bold = true })
+vim.api.nvim_set_hl(0, "CmpItemAbbrMatchFuzzy", { fg = "#82AAFF", bg = "NONE", bold = true })
+vim.api.nvim_set_hl(0, "CmpItemMenu", { fg = "#C792EA", bg = "NONE", italic = true })
+
+vim.api.nvim_set_hl(0, "CmpItemKindField", { fg = "#FFFFFF", bg = "#B5585F" })
+vim.api.nvim_set_hl(0, "CmpItemKindProperty", { fg = "#FFFFFF", bg = "#B5585F" })
+vim.api.nvim_set_hl(0, "CmpItemKindEvent", { fg = "#FFFFFF", bg = "#B5585F" })
+
+vim.api.nvim_set_hl(0, "CmpItemKindText", { fg = "#FFFFFF", bg = "#6d8b41" })
+vim.api.nvim_set_hl(0, "CmpItemKindEnum", { fg = "#FFFFFF", bg = "#6d8b41" })
+vim.api.nvim_set_hl(0, "CmpItemKindKeyword", { fg = "#FFFFFF", bg = "#6d8b41" })
+
+vim.api.nvim_set_hl(0, "CmpItemKindConstant", { fg = "#FFFFFF", bg = "#cccc00" })
+vim.api.nvim_set_hl(0, "CmpItemKindConstructor", { fg = "#FFFFFF", bg = "#cccc00" })
+vim.api.nvim_set_hl(0, "CmpItemKindReference", { fg = "#FFFFFF", bg = "#cccc00" })
+
+vim.api.nvim_set_hl(0, "CmpItemKindFunction", { fg = "#FFFFFF", bg = "#A377BF" })
+vim.api.nvim_set_hl(0, "CmpItemKindStruct", { fg = "#FFFFFF", bg = "#A377BF" })
+vim.api.nvim_set_hl(0, "CmpItemKindClass", { fg = "#FFFFFF", bg = "#A377BF" })
+vim.api.nvim_set_hl(0, "CmpItemKindModule", { fg = "#FFFFFF", bg = "#A377BF" })
+vim.api.nvim_set_hl(0, "CmpItemKindOperator", { fg = "#FFFFFF", bg = "#A377BF" })
+
+vim.api.nvim_set_hl(0, "CmpItemKindVariable", { fg = "#FFFFFF", bg = "#7E8294" })
+vim.api.nvim_set_hl(0, "CmpItemKindFile", { fg = "#FFFFFF", bg = "#7E8294" })
+
+vim.api.nvim_set_hl(0, "CmpItemKindUnit", { fg = "#FFFFFF", bg = "#cc9900" })
+vim.api.nvim_set_hl(0, "CmpItemKindSnippet", { fg = "#FFFFFF", bg = "#cc9900" })
+vim.api.nvim_set_hl(0, "CmpItemKindFolder", { fg = "#FFFFFF", bg = "#cc9900" })
+
+vim.api.nvim_set_hl(0, "CmpItemKindMethod", { fg = "#FFFFFF", bg = "#6C8ED4" })
+vim.api.nvim_set_hl(0, "CmpItemKindValue", { fg = "#FFFFFF", bg = "#6C8ED4" })
+vim.api.nvim_set_hl(0, "CmpItemKindEnumMember", { fg = "#FFFFFF", bg = "#6C8ED4" })
+
+vim.api.nvim_set_hl(0, "CmpItemKindInterface", { fg = "#FFFFFF", bg = "#58B5A8" })
+vim.api.nvim_set_hl(0, "CmpItemKindColor", { fg = "#FFFFFF", bg = "#58B5A8" })
+vim.api.nvim_set_hl(0, "CmpItemKindTypeParameter", { fg = "#FFFFFF", bg = "#58B5A8" })
+
 
 return {
     formatting = {
-        format = require("lspkind").cmp_format({
-            mode = 'symbol_text', -- show only symbol annotations
-            maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-                           -- can also be a function to dynamically calculate max width such as 
-                           -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
-            ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-            show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+        fields = { "kind", "abbr", "menu" },
+        format = function(entry, vim_item)
+            local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
+            local strings = vim.split(kind.kind, "%s", { trimempty = true })
+            kind.kind = " " .. (strings[1] or "") .. " "
+            kind.menu = "    (" .. (strings[2] or "") .. ")"
 
-            -- The function below will be called before any actual modifications from lspkind
-            -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-            before = function (entry, vim_item)
-                return vim_item
-            end
-        })
+            return kind
+        end,
     },
     window = {
         completion = {
-            border = "rounded",
             winhighlight = "Normal:CmpNormal",
+            col_offset = -3,
+            side_padding = 0,
         }
     },
     mapping = cmp.mapping.preset.insert({
