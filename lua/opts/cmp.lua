d803fb702bd558735e7d6a47fe8def1d23f71d0a
Joachim-barre <joachim.barre22@laposte.net>
2024-08-16 14:56:11 +0200

    switched to symbol text for lspkind

--- lua/opts/cmp.lua
+++ lua/opts/cmp.lua
@@ -28,7 +28,7 @@ vim.opt.completeopt = "menu,menuone"
 return {
     formatting = {
         format = require("lspkind").cmp_format({
-            mode = 'symbol', -- show only symbol annotations
+            mode = 'symbol_text', -- show only symbol annotations
             maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
                            -- can also be a function to dynamically calculate max width such as 
                            -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
