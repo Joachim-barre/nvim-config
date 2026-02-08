70212382ebd4e44fdb14f3d818bc08ae8b6586f2
Joachim-barre <joachim.barre22@laposte.net>
2026-02-08 14:04:13 +0100

    fix for new lspkind version

--- lua/opts/cmp.lua
+++ lua/opts/cmp.lua
@@ -86,8 +86,8 @@ return {
         format = function(entry, vim_item)
             local kind = lspkind.cmp_format({ mode = "symbol_text", maxwidth = 50, symbol_map = overiden_icons })(entry, vim_item)
             local strings = vim.split(kind.kind, "%s", { trimempty = true })
-            kind.kind = " " .. (strings[1] or "") .. " "
-            kind.menu = "   [" .. entry.source.name .. "] ("  .. (strings[2] or "") .. ")"
+            kind.kind = " " .. kind.icon .. "  "
+            kind.menu = "   [" .. entry.source.name .. "] ("  .. (strings[1] or "") .. ")"
 
             return kind
         end,
