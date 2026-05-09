47f82312e9af770914cd0a8645ee50664c75eeed
Joachim-barre <joachim.barre22@laposte.net>
2026-05-09 10:28:00 +0200

    added another hop movement

--- lua/opts/hop.lua
+++ lua/opts/hop.lua
@@ -4,6 +4,10 @@ vim.keymap.set("n", "f", function()
     hop.hint_words()
 end)
 
+vim.keymap.set("n", "w", function()
+    hop.hint_lines({ multi_windows = true })
+end)
+
 return {
     uppercase_labels = true,
 }
